e49f23540e311b69a1c9fc05dbfa413d22105fba
YueFan Wu <13456147198@163.com>
2020-08-29 15:03:48 +1000

    demo for testing github actions
    change static file structure
    add dummy request mapping

--- src/main/java/com/eportfolio2077/eportfolio/EportfolioApplication.java
+++ src/main/java/com/eportfolio2077/eportfolio/EportfolioApplication.java
@@ -2,16 +2,17 @@ package com.eportfolio2077.eportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class EportfolioApplication {
 
-    @RequestMapping("index")
-    public String index(){
-        return "hello";
+    @RequestMapping("login")
+    public String login(){
+        return "/html/login.html";
     }
 
 
---
+++ src/main/java/com/eportfolio2077/eportfolio/Service/service.java
@@ -0,0 +1,4 @@
+package com.eportfolio2077.eportfolio.Service;
+
+public class service {
+}
