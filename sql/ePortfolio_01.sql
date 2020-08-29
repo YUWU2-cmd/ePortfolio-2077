6804275e64e885c37f55d8ae1d04e144857c89cf
Tianyu Xiang <xiangt1@student.unimelb.edu.au>
2020-08-29 20:58:13 +1000

    database sql & ER diagram version1
    add database sql & ER diagram version1 (login&register)

---
+++ sql/ePortfolio_01.sql
@@ -0,0 +1,42 @@
+create database eportfolio;
+
+create table User
+(
+    UserId int
+    AUTO_INCREMENT PRIMARY KEY,
+    UserName varchar
+    (100) NOT NULL,
+    Email varchar
+    (100) NOT NULL,
+    Password varchar
+    (30) NOT NULL
+);
+
+    create table Role
+    (
+        RoleId int
+        AUTO_INCREMENT PRIMARY KEY,
+    Name varchar
+        (100),
+    Description varchar
+        (100)
+);
+
+        create table UserRole
+        (
+            UserRoleId int
+            AUTO_INCREMENT PRIMARY KEY,
+    UserId int,
+    RoleId int,
+    CONSTRAINT 'fk_user' FOREIGN KEY
+            (UserId) REFERENCES User
+            (UserId),
+    CONSTRAINT 'fk_role' FOREIGN KEY
+            (RoleId) REFERENCES Role
+            (RoleId)
+);
+
+
+
+
+
