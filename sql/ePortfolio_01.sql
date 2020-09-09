71949c329293a9d9eed42191872a5b0f6e50c4ad
Tianyu Xiang <xiangt1@student.unimelb.edu.au>
2020-09-09 19:57:57 +1000

    added Image/Blog
    Added Image/Blog
    Update User

--- sql/ePortfolio_01.sql
+++ sql/ePortfolio_01.sql
@@ -2,44 +2,60 @@ create database eportfolio;
 
 create table User
 (
-    UserId int
-    AUTO_INCREMENT PRIMARY KEY,
-    UserName varchar
-    (100) NOT NULL,
-    Email varchar
-    (100) NOT NULL,
-    Password varchar
-    (30) NOT NULL
+    UserId int   AUTO_INCREMENT PRIMARY KEY,
+    UserName varchar(100)  NOT NULL,
+    Email varchar(100)  NOT NULL,
+    Password varchar(30)  NOT NULL
 );
 
     create table Role
     (
-        RoleId int
-        AUTO_INCREMENT PRIMARY KEY,
-    Name varchar
-        (100),
-    Description varchar
-        (100)
+        RoleId int  AUTO_INCREMENT PRIMARY KEY,
+        Name varchar(100),
+        Description varchar(100)
 );
 
-        create table UserRole
-        (
-            UserRoleId int
-            AUTO_INCREMENT PRIMARY KEY,
+create table UserRole (
+	UserRoleId int AUTO_INCREMENT PRIMARY KEY,
     UserId int,
     RoleId int
 );
 
 alter table UserRole 
 add constraint fk_role
-foreign key(RoleId) 
-references Role(RoleId);
+foreign key(RoleId) references Role(RoleId);
 
 alter table UserRole 
 add constraint fk_user 
-foreign key(UserId) 
-references User(UserId);
+foreign key(UserId)  references User(UserId);
 
+create table Blog(
+	BlogId int auto_increment primary key,
+    Title varchar(200) not null,
+    Content text,
+    CreatedTime datetime,
+    UserId int    
+);
 
+alter table Blog 
+add constraint fk_blog_user
+foreign key(UserId) references User(UserId);
+
+alter table User
+add column bio varchar(500);
+
+create table Image(
+	ImageId int auto_increment primary key,
+    ImageName varchar(200),
+    ImagePath varchar(200),
+    UserId int
+);
+
+alter table Image
+add constraint fk_image_user
+foreign key(UserId) references User(UserId);
+ 
+alter table Image
+ add unique unique_name_path (ImageName,ImagePath);
 
 
