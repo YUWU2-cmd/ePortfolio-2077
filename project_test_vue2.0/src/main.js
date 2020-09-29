1c5baa9c7e7c7be886d6168c1b7b5c906e112673
yuxxie769 <69099903+yuxxie769@users.noreply.github.com>
2020-09-29 16:49:07 +1000

    upload test project

---
+++ project_test_vue2.0/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+
+// 手动配置 element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/element.js'
+
+import axios from 'axios'
+import Qs from 'qs'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置请求拦截器，给请求加上token
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+
+Vue.prototype.$http = axios
+Vue.prototype.$qs = Qs
+
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
