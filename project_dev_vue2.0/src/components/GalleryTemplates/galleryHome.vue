2bd88dfab0edc109774ef97d4d4f2229454b139d
yuwu2 <yuwu2@student.unimelb.edu.au>
2020-09-30 11:04:11 +0800

    fix gallery template display issues

--- project_dev_vue2.0/src/components/GalleryTemplates/galleryHome.vue
+++ project_dev_vue2.0/src/components/GalleryTemplates/galleryHome.vue
@@ -88,21 +88,23 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+body{
+    min-width: 1050px;
+}
 #home-body .body-wrapper{
    width: 100%;
    height: 3130px;
 }
 
 #home-body .content-wrapper{
-    width: 70%;
+    width: 1050px;
     padding-top: 20px;
-    margin-left: 15%;
     height: 3130px;
+    margin: 0 auto;
 }
 #home-body .content-wrapper .left-content{
     float: left;
     width: 500px;
-    margin-left: 50px;
     display: flex;
     flex-direction: column;
 }
