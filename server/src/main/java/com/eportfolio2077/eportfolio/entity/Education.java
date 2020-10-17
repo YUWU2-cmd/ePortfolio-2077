0bc13482634e9f52668e20aee7e2ce9729b95a8f
Jeff <jeff666666666666666@gmail.com>
2020-10-17 18:57:21 +1100

    attributes change following DB

--- server/src/main/java/com/eportfolio2077/eportfolio/entity/Education.java
+++ server/src/main/java/com/eportfolio2077/eportfolio/entity/Education.java
@@ -2,8 +2,10 @@ package com.eportfolio2077.eportfolio.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
@@ -12,12 +14,7 @@ public class Education {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long educationId;
 
-    @Temporal(TemporalType.DATE)
-    private Date startTime;
-
-    @Temporal(TemporalType.DATE)
-    private Date EndTime;
-
+    private String duration;
     private String schoolName;
     private String degree;
     private String major;
