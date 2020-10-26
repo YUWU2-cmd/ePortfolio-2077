f63fe6495f40f22500b61fcb83e6085b9527618b
YueFan Wu <13456147198@163.com>
2020-10-26 22:04:22 +0800

    update userdto

--- server/src/main/java/com/eportfolio2077/eportfolio/dto/UserDto.java
+++ server/src/main/java/com/eportfolio2077/eportfolio/dto/UserDto.java
@@ -7,9 +7,23 @@ import lombok.Data;
 public class UserDto {
     private String username;
     private String profilePic;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String linkedinLink;
+    private String facebookLink;
+    private String twitterLink;
+    private String instagramLink;
 
     public UserDto(User user){
         this.username=user.getUsername();
         this.profilePic=user.getProfilePicture();
+        this.firstName=user.getFirstName();
+        this.lastName=user.getLastName();
+        this.phoneNumber=user.getPhoneNumber();
+        this.linkedinLink=user.getLinkedinLink();
+        this.facebookLink=user.getFacebookLink();
+        this.twitterLink=user.getTwitterLink();
+        this.instagramLink=user.getInstagramLink();
     }
 }
